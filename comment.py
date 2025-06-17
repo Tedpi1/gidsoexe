9c3be726140efce232f23186d16e7c9e9dc63713
Tedpi <moroted16@gmail.com>
2025-06-17 19:58:42 +0300

    teddy

---
+++ comment.py
@@ -0,0 +1,20 @@
+age = 25
+name = "25"
+time = 30
+is_user = True
+temp = 3.5
+# print(time + age)
+
+say = "to is"
+
+# print(say + " " + str(age))
+# str()- converts the age to a string
+# float() -converts to float
+# bool() -converts to boolean
+
+# print(temp + bool(age))
+
+c = bool(0)
+print(c)
+b = bool(1)
+print(b)
---
+++ list.py
@@ -0,0 +1,27 @@
+# defining a list
+my_list = [1, "apples", 3.14, True]
+
+# accessing elements
+# print(my_list[0])  # Output: 1
+
+# # slicing the list
+# print(my_list[2:3])  # Output: ['apples', 3.14]
+
+# adding an element
+# my_list.append("Guava")
+# print(my_list)
+
+# my_list.append(1)
+# print(my_list)
+
+# # insert to a specifi index
+# my_list.insert(1, "Banana")
+# print(my_list)
+
+# # removing an element
+# my_list.remove("apples")
+# print(my_list)
+
+# remove using pop
+my_list.pop()
+print(my_list)
